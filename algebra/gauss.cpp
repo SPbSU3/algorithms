6334b5276daa8584a2ca4d41c51ce6e81e11087a
Aleksandr Logunov <sinesight@gmail.com>
2018-04-05 20:07:31 +0300

    Update gauss.cpp

--- algebra/gauss.cpp
+++ algebra/gauss.cpp
@@ -1,13 +1,19 @@
+using ld = long double;
+const ld eps = 1e-9;
+
+// Returns vector of solutions of the given linear system.
+// Result is empty iff the system is incompatible.
+// Otherwise the result equals {(x[i], type[i])}_i, where type[i] = true <=> value of x[i] is fixed.
 vector<pair<ld, int>> gauss(vector<vector<ld>> a) {
-	vector<pair<ld, int>> res; //res[i].fst = x[i];
-	                            //res[i].snd = 1 <=> value of x[i] is fixed; 0 otherwise
+	vector<pair<ld, bool>> res; 
 	
 	int n = sz(a);
-	if (n == 0)
+	if (n == 0) {
 		return res;
+	}
 	int m = sz(a[0]) - 1;
 	
-	vector<int> where;
+	vi where;
 	res.resize(m, {0, 0});
 	where.resize(m, -1);	
 	
@@ -24,28 +30,31 @@ vector<pair<ld, int>> gauss(vector<vector<ld>> a) {
 			swap(a[row][j], a[id][j]);
 		where[col] = row;	
 		
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < n; j++) {
 			if (j != row) {
 				ld C = -a[j][col] / a[row][col];
-				for (int k = 0; k <= m; k++)
+				for (int k = 0; k <= m; k++) {
 					a[j][k] += a[row][k] * C;	
+				}
 			}
+		}
 			
 		row++;
 	}
 	
 	//assigning
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < m; i++) {
 		if (where[i] != -1) {
-			res[i].X = a[where[i]][m] / a[where[i]][i];
-			res[i].Y = 1;	
+			res[i] = {a[where[i]][m] / a[where[i]][i], 1};
 		}
+	}
 		
 	//checking
 	for (int i = 0; i < n; i++) {
 		ld sum = 0;
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < m; j++) {
 			sum += res[j].fst * a[i][j];
+		}
 		if (fabs(sum - a[i][m]) >= eps) {
 			res.clear();
 			return res;
